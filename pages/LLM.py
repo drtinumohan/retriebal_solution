47dab97ff9c0daca4c15287cfc1523e9ecefeb39
tinu mohan <tinumohan2014@gmail.com>
2023-07-14 17:40:13 +0530

    llm

--- pages/LLM.py
+++ pages/LLM.py
@@ -31,49 +31,24 @@ def setup_model(doc_list):
     document_chunks = get_files_chunks(doc_search_model.tokenizer, text)
     doc_embd = doc_search_model.get_document_embedding(document_chunks)
     return doc_embd, document_chunks, doc_search_model
-# result = " ".join([document_chunks[doc_info[0]] for doc_info in topk_docs])
-# st.write(result)
-# llm_pipeline = get_pipeline()
-# llm = HuggingFacePipeline(pipeline = llm_pipeline)
 
-# emb_model, doc_emb, doc_text, index_model  = setup_embed_model(doc_list, model_config.get(sematic_search_key_name, default_sematic_search_model_name))
+
+llm_pipeline = get_pipeline()
+llm = HuggingFacePipeline(pipeline = llm_pipeline)
 
 
 
 
 
-# template = """
-# read the  below paragraphs and answer the question in a few words
-# context: {context} 
-# answer of the question {question} 
-# """
-# prompt = PromptTemplate(template=template, input_variables=["question","context"])
 
-# llm_chain = LLMChain(prompt=prompt, llm=llm)
+template = """
+read the  below paragraphs and answer the question in a few words
+context: {context} 
+answer of the question {question} 
+"""
+prompt = PromptTemplate(template=template, input_variables=["question","context"])
 
-# # context = """"
-# # "State Film Awards (1986)" . Department of Information and Public Relations. Archived from theoriginal on 19 November 2009. Retrieved6 May 2012. Awards See accolades Actor, a Special Jury Mention and a Special Jury Awardfor acting, and an award for Best Feature Film (asproducer), also nine Kerala State Film Awards and 17. Khan, Ujala Ali (14 September 2013). "Reigning southern stars" . The National. Archived from the original on 13 January 2017. Retrieved 11 January 2017. 112. Punnoose, Aby (11 November 2021). "67th-national-film-awards-marakkar-arabikkadalinte-simham-wins-the-best-feature-film-award" . Timesofindia. Retrieved 11 November2021.
-# # """ 
-# # question = "when mohanlal got kerala state award"
-# # prompt.format(question=question,context= context)
-
-
-
-# # context = st.text_area(
-# #     "context",
-# #     value='',
-# #     height=300,
-# #     max_chars=1500,
-# #     key=None,
-# #     help=None,
-# #     on_change=None,
-# #     args=None,
-# #     kwargs=None,
-# #     placeholder=None,
-# #     disabled=False,
-# #     label_visibility="visible",
-# # )
-
+llm_chain = LLMChain(prompt=prompt, llm=llm)
 doc_embd, document_chunks, doc_search_model = setup_model(doc_list)
 question = st.text_input(
     "question",

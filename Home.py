52d870b375231e8cfd4fd1e63c67f3ff98d113c8
tinu mohan <tinumohan2014@gmail.com>
2023-07-15 08:45:29 +0530

    "updates"

--- Home.py
+++ Home.py
@@ -13,16 +13,27 @@ from utils.constant import (
 
 st.title("Model selection")
 
-with st.form(
-    key="model_selection", clear_on_submit=True
-):  # st.form(key="Form :", clear_on_submit = True):
-    sematic_search_model_name = st.selectbox(
-        "Select Semantic Search Model", sematic_search_model_names
-    )
-    qa_model_name = st.selectbox("QA Model", qa_model_names)
-    config_Submit = st.form_submit_button(label="Submit")
 
-if config_Submit:
+if "sematic_search_model_name" not in st.session_state:
+    st.session_state.sematic_search_model_name = 0
+
+if "qa_model_name" not in st.session_state:
+    st.session_state.qa_model_name = 0
+
+
+sematic_search_model_name = st.selectbox(
+    "Select Semantic Search Model", 
+    sematic_search_model_names,
+    index= st.session_state.sematic_search_model_name
+)
+qa_model_name = st.selectbox("QA Model", 
+                                qa_model_names,
+                            index= st.session_state.qa_model_name
+                                )
+config_Submit = st.button(label="Submit")
+
+if config_Submit and sematic_search_model_name !=  "<select>" and qa_model_name!=   "<select>":
+    st.session_state.sematic_search_model_name = sematic_search_model_names.index(sematic_search_model_name)
     save_path = Path(folder_location, model_config_fname)
     model_config = {
         sematic_search_key_name: sematic_search_model_name,
@@ -32,4 +43,8 @@ if config_Submit:
         f"model {sematic_search_model_name} and {qa_model_name} is successfully saved!"
     )
     save_json(save_path, model_config)
-    # st.write(model_config)
+elif config_Submit:
+    st.write("select above configrations")
+else:
+    pass
+
